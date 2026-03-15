f858b9efd32fb7ba0a0ded67bffe7f72be764a62
Cloe Alberto de Souza <cloe.alberto@freemann.com.br>
2026-03-15 20:38:02 -0300

    refactor: melhorar documentação da classe Conteudo e adicionar método toString

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,18 +1,33 @@
 package br.com.dio.desafio.dominio;
 
+/**
+ * Classe abstrata que representa um conteúdo educacional.
+ * Não pode ser instanciada diretamente, apenas herdada por outras classes
+ * como Curso ou Mentoria.
+ */
 public abstract class Conteudo {
 
+    /**
+     * Constante de XP base usada no cálculo de experiência.
+     * static = pertence à classe
+     * final = não pode ser alterada
+     */
     protected static final double XP_PADRAO = 10d;
 
     private String titulo;
     private String descricao;
 
+    /**
+     * Método abstrato que obriga as classes filhas
+     * a implementarem sua própria lógica de cálculo de XP.
+     */
     public abstract double calcularXp();
 
     public String getTitulo() {
         return titulo;
     }
 
+    // Setter responsável por definir o título do conteúdo
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
@@ -21,7 +36,20 @@ public abstract class Conteudo {
         return descricao;
     }
 
+    // Setter responsável por definir a descrição do conteúdo
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    /**
+     * Sobrescrita do método toString para facilitar debug
+     * e visualização do objeto em logs.
+     */
+    @Override
+    public String toString() {
+        return "Conteudo{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
     }
 }
